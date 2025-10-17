c64263a17dc3fe6a447d303a93669b40c9e016ff
Vadim Savin <savinvadim1312@gmail.com>
2025-10-17 16:36:36 +0200

    Native bottom tabs

--- src/app/(drawer)/(home)/(tabs)/_layout.tsx
+++ src/app/(drawer)/(home)/(tabs)/_layout.tsx
@@ -1,19 +1,20 @@
-import { Tabs } from 'expo-router';
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name='index'
-        options={{
-          title: 'Chat',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name='chatbox' size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen name='settings' options={{ title: 'Setting' }} />
-    </Tabs>
+    <NativeTabs>
+      <NativeTabs.Trigger name='chats'>
+        <Label>Chats</Label>
+        <Icon sf='message.fill' drawable='custom_android_drawable' />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name='settings'>
+        <Icon sf='gear' drawable='custom_settings_drawable' />
+        <Label>Settings</Label>
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name='search' role='search'>
+        <Label>Search</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
